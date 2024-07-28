a4019c70e4cd1278e914211d8d17e1d89ff48d88
Mohitscirpt <157406484+Mohitscirpt@users.noreply.github.com>
2024-07-28 18:43:14 +0000

    variable and datatype

---
+++ baisc/datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all JS code as newer version
+//alert(3+3) we are using nodejs ,not browser
+
+//code should be readability be high
+
+console.log(3
+    +4
+);
+
+//number =>2 to powe 53
+// bigint
+// string=>""
+//boolean=>true/false
+//null=>standalone value
+// undefined
+// symbol=>unique
+
+//object
+console.log(typeof null);
---
+++ baisc/variable.js
@@ -0,0 +1,11 @@
+const accountId=14523
+let accountEmail="mohit@gmail.com"
+var accountPassword="1234"
+accountCity="Jaipur"
+let accountState;
+
+/*
+prefer not use a var
+because if issue in block scope in function scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
