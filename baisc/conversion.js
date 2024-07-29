fb9c567e3cf0c4ec913167824294c25490e1d1d3
Mohitscirpt <157406484+Mohitscirpt@users.noreply.github.com>
2024-07-29 17:52:34 +0000

    conversion

---
+++ baisc/conversion.js
@@ -0,0 +1,17 @@
+// let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+// let value=Number(score)
+// console.log(typeof value)
+// console.log(value);
+
+// "33"=>number
+//"33avc"=>NaN
+// true =>1, false=>0
+
+let isLoginpage=''
+let theValueValid=Boolean(isLoginpage)
+// console.log(theValueValid);
+let somenumber=9
+let thevalid=String(somenumber)
+console.log(thevalid);
