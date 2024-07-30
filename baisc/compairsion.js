cabbd99b7c1189f28dc94006db9f9bb4fb843c85
Mohitscirpt <157406484+Mohitscirpt@users.noreply.github.com>
2024-07-30 17:50:13 +0000

    comparison

---
+++ baisc/compairsion.js
@@ -0,0 +1,14 @@
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null<0);
+// console.log(null<=0);
+// console.log(null==0);
+/*
+only for interview ask the question what  the output
+*/
+
+console.log(undefined==0);
+console.log(undefined<=0);
+console.log(undefined>=0);
+console.log(undefined>0);
+console.log(undefined<0);
