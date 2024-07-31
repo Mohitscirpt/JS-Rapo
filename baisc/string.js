085175b123d3f5418022275346a27d5064559ee4
Mohitscirpt <157406484+Mohitscirpt@users.noreply.github.com>
2024-07-31 18:25:17 +0000

    String

---
+++ baisc/string.js
@@ -0,0 +1,16 @@
+const name ="Mohit"
+const newrepo=50
+console.log(`hello is this is ${name} the repo is the count ${newrepo}`)
+
+
+const game = new String("marvel")
+console.log(game.__proto__);
+console.log(game.length)
+console.log(game.toUpperCase());
+console.log(game.charAt(3)); 
+
+const newgame = game.substring(0,4)
+console.log(newgame)
+
+const newgameamother = game.slice(-8,4)
+console.log(newgameamother)
