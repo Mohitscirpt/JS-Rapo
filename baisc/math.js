607aea56e079d6ad51ee614f13c1b4e088c61193
Mohitscirpt <157406484+Mohitscirpt@users.noreply.github.com>
2024-08-03 18:20:15 +0000

    math fn

---
+++ baisc/math.js
@@ -0,0 +1,29 @@
+// // const number=100
+// // console.log(number);
+
+// // const mm =  new Number(100)
+// // console.log(mm);
+// // console.log(mm.toString().length)
+// // console.log(mm.toFixed(3))
+
+// // const other = 123.899
+// // console.log(other.toPrecision(3))
+
+
+// // const mydost = 123456789
+// // console.log(mydost.toLocaleString())
+
+// console.log(Math)
+// console.log(Math.abs(-9))
+// console.log(Math.round(4.6))
+// console.log(Math.round(4.2))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.8))
+// console.log(Math.max(1,23,8,500))
+// console.log(Math.min(1,23,8,500))
+
+
+console.log(Math.random())
+const max = 64
+const min =2
+console.log(Math.random()-min*max+1+0.56456+Math.random())
